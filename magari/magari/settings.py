6fd63e2776b7f264e8e27f56bf88b242c533cf7e
Erick Awino <erick.otieno89@gmail.com>
2024-10-31 14:26:23 +0300

    added gitignore and dotenv

--- magari/magari/settings.py
+++ magari/magari/settings.py
@@ -149,3 +149,7 @@ import os
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
